2d7697b587e84f8ea0313da27b44a568c933bfb8
cailizheng <cailizheng@postoa.com.cn>
2019-02-17 11:28:37 +0800

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 
 
-		System.out.println("*******************");
+		System.out.println("*********ssss**********");
 	}
 
 }
